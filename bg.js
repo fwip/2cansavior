a21bcb592745966c60490f31bd003edf803a8255
Jem <pink.fwip@gmail.com>
2012-07-15 18:13:30 -0400

    Fix user-name filtering.

--- bg.js
+++ bg.js
@@ -1,7 +1,7 @@
 chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.greeting === "getusers"){
-            sendResponse({users: localStorage.evilusers});
+            sendResponse({users: localStorage.evilusers.split(',')});
         }
     }
     );
